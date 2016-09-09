0e4e54174a2a038f6519d8235e499b86a382a8eb
Paweł Obrok <pawel.obrok@gmail.com>
2016-09-09 22:31:48 +0200

    Start splitting Main

--- src/Main.hs
+++ src/Main.hs
@@ -12,6 +12,7 @@ import Linear (V2(..))
 import GHC.Exts
 import Control.Monad
 import Data.Foldable
+import qualified Window
 
 data Drawable = Drawable Int Int Representation
 
@@ -32,22 +33,7 @@ tex = SField
 
 main :: IO ()
 main = do
-  True <- GLFW.init
-  Just primary <- GLFW.getPrimaryMonitor
-  Just VideoMode{videoModeHeight, videoModeWidth, videoModeRedBits, videoModeGreenBits, videoModeBlueBits, videoModeRefreshRate} <- GLFW.getVideoMode primary
-  GLFW.windowHint (WindowHint'ContextVersionMajor 3)
-  GLFW.windowHint (WindowHint'ContextVersionMinor 2)
-  GLFW.windowHint (WindowHint'OpenGLProfile OpenGLProfile'Core)
-  GLFW.windowHint (WindowHint'OpenGLForwardCompat True)
-  GLFW.windowHint (WindowHint'RedBits videoModeRedBits)
-  GLFW.windowHint (WindowHint'GreenBits videoModeGreenBits)
-  GLFW.windowHint (WindowHint'BlueBits videoModeBlueBits)
-  GLFW.windowHint (WindowHint'AlphaBits 8)
-  GLFW.windowHint (WindowHint'RefreshRate videoModeRefreshRate)
-  GLFW.windowHint (WindowHint'ClientAPI ClientAPI'OpenGL)
-  Just window <- GLFW.createWindow videoModeWidth videoModeHeight "DIVE" (Just primary) Nothing
-  GLFW.makeContextCurrent (Just window)
-  GLFW.swapInterval 1
+  window <- Window.init
 
   (x, y) <- GLFW.getWindowSize window
   let tiler = tile (x `div` tileSize) (y `div` tileSize)
---
+++ src/Window.hs
@@ -0,0 +1,24 @@
+module Window where
+
+import Graphics.UI.GLFW as GLFW
+
+init :: IO Window
+init = do
+  True <- GLFW.init
+  Just primary <- GLFW.getPrimaryMonitor
+  Just VideoMode{videoModeHeight, videoModeWidth, videoModeRedBits, videoModeGreenBits, videoModeBlueBits, videoModeRefreshRate} <- GLFW.getVideoMode primary
+  GLFW.windowHint (WindowHint'ContextVersionMajor 3)
+  GLFW.windowHint (WindowHint'ContextVersionMinor 2)
+  GLFW.windowHint (WindowHint'OpenGLProfile OpenGLProfile'Core)
+  GLFW.windowHint (WindowHint'OpenGLForwardCompat True)
+  GLFW.windowHint (WindowHint'RedBits videoModeRedBits)
+  GLFW.windowHint (WindowHint'GreenBits videoModeGreenBits)
+  GLFW.windowHint (WindowHint'BlueBits videoModeBlueBits)
+  GLFW.windowHint (WindowHint'AlphaBits 8)
+  GLFW.windowHint (WindowHint'RefreshRate videoModeRefreshRate)
+  GLFW.windowHint (WindowHint'ClientAPI ClientAPI'OpenGL)
+  Just window <- GLFW.createWindow videoModeWidth videoModeHeight "DIVE" (Just primary) Nothing
+  GLFW.makeContextCurrent (Just window)
+  GLFW.swapInterval 1
+
+  return window
