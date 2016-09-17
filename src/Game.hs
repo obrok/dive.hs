364ad203872ebca628cc6f550f0142712aa951f1
Paweł Obrok <pawel.obrok@gmail.com>
2016-09-17 15:45:22 +0200

    Refactor movement

--- src/Game.hs
+++ src/Game.hs
@@ -1,6 +1,6 @@
 module Game where
 
-import Graphics.UI.GLFW
+import Graphics.UI.GLFW (Key(..))
 
 data Character = Character Int Int
   deriving (Show)
@@ -17,11 +17,15 @@ data Status = Alive | Dead
 data State = State { character :: Character, mobs :: Mobs }
   deriving (Show)
 
+newtype GameTime = GameTime Int
+
 initialState :: State
 initialState = State (Character 0 0) (Mobs [Mob 10 10 Alive])
 
 updateState :: Key -> State -> State
-updateState key state = nextState key state state
+updateState key state =
+  let (time, state') = applyAction key state
+    in nextState time state'
 
 getCharacter :: State -> Character
 getCharacter State { character } = character
@@ -32,23 +36,12 @@ getMobs State { mobs = Mobs ms } = ms
 getWalls :: State -> [(Int, Int)]
 getWalls _ = [(20, 20), (20, 21), (21, 20)]
 
-class UpdateOnInput a where
-  nextState :: Key -> State -> a -> a
+nextState :: GameTime -> State -> State
+nextState _time state = state
 
-instance UpdateOnInput State where
-  nextState key state (State character mobs) =
-    State (nextState key state character) (nextState key state mobs)
-
-instance UpdateOnInput Mobs where
-  nextState k s (Mobs mobs) = Mobs $ map (nextState k s) mobs
-
-instance UpdateOnInput Mob where
-  nextState _ (State (Character x1 y1) _) (Mob x2 y2 Alive) | x1 == x2 && y1 == y2 = Mob x2 y2 Dead
-  nextState _ _ mob = mob
-
-instance UpdateOnInput Character where
-  nextState Key'Up _ (Character x y) = Character x (y + 1)
-  nextState Key'Down _ (Character x y) = Character x (y - 1)
-  nextState Key'Right _ (Character x y) = Character (x + 1) y
-  nextState Key'Left _ (Character x y) = Character (x - 1) y
-  nextState _ _ x = x
+applyAction :: Key -> State -> (GameTime, State)
+applyAction Key'Up state@State {character = (Character x y)} = (GameTime 0, state{character = Character x (y + 1)})
+applyAction Key'Down state@State {character = (Character x y)} = (GameTime 0, state{character = Character x (y - 1)})
+applyAction Key'Left state@State {character = (Character x y)} = (GameTime 0, state{character = Character (x - 1) y})
+applyAction Key'Right state@State {character = (Character x y)} = (GameTime 0, state{character = Character (x + 1) y})
+applyAction _ x = (GameTime 0, x)
